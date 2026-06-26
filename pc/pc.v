f5d8acb079532175530e85d25ac036eb62113e3b
Taimoor Iqbal <taimoor.ij@gmail.com>
2026-06-26 11:18:34 -0400

    Phase 3: program counter with synchronous reset

---
+++ pc/pc.v
@@ -0,0 +1,15 @@
+module pc (
+    input             clk,
+    input             reset,
+    input      [31:0] next_pc,
+    output reg [31:0] pc
+);
+
+    // clocked update:
+    always @(posedge clk)
+        if (reset) 
+            pc <= 32'b0; // reset high -> pc <= address 0
+        else
+            pc <= next_pc; // otherwise  -> pc <= next_pc
+
+endmodule
